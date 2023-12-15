aac68845b32a031be185d665ba8d8a32af6f5070
Diego Fabbri <diegofabbri.dev@gmail.com>
2023-12-15 00:11:09 -0300

    feat: add header component

---
+++ src/components/header.tsx
@@ -0,0 +1,12 @@
+export function Header() {
+  return(
+    <header className="py-7 flex justify-center">
+      <p className="text-4xl text-neutral-100 font-display">
+        meta.
+        <span className="text-[#45EDAD]">
+          diária
+        </span>
+      </p>
+    </header>
+  )
+}
